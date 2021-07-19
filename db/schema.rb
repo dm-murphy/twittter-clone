fd368e933b4dad76c24596f487593f9dcaceb6ca
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-07-18 23:23:11 -0400

    Modify database from sqlite3 to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_14_212145) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
